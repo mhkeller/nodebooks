aaaae0e322d6793cd448599f0cca522a1ffc9845
mhkeller <michael.keller@gmail.com>
2015-08-24 00:24:00 -0400

    comments

--- nodebooks.js
+++ nodebooks.js
@@ -8,7 +8,6 @@ var peeks = []
 // Read in our script and parse it
 var script = fs.readFileSync('./index.js', 'utf-8')
 var tree = acorn.parse(script, {locations: true})
-// console.log(tree)
 
 // How many calls to the `display` function do we have?
 var display_calls = tree.body.filter(function(obj){
@@ -21,13 +20,14 @@ var showPeeks_after = _.after(numb_display_calls, showPeeks)
 
 var script_lines = script.split('\n')
 
+// Starting from the bottom, work backwards inserting our stringified data in the line after the calls to `display`
+// Find out which one to display based on its index
 function showPeeks(){
   for (var i = display_calls.length - 1; i >= 0; i--) {
     var display_call = display_calls[i]
     var script_line_index = display_call.loc.end.line - 1
     var associated_peek = '````\n\nValue is: ' + JSON.stringify(peeks[i]) + '\n\n' // insert transformation step, simply stringify for now
     
-    console.log(i, display_calls.length - 1)
     if (i < display_calls.length - 1) {
       associated_peek += '````js'
     }
@@ -39,7 +39,6 @@ function showPeeks(){
 
 function keepTrack(obj) {
   peeks.push(obj)
-  // console.log(numb_display_calls)
   showPeeks_after()
 }
 

7f3a9136f3ee734d608133b10316f86f5d6ba023
Rich-Harris <richard.a.harris@gmail.com>
2015-08-25 21:18:12 -0400

    handle non-top-level display calls

---
+++ bin/nodebooks.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+var nodebooks = require('../nodebooks')
+
+var file = process.argv[2]
+console.log( file )
+
+function formatPeek ( peek ) {
+  return peek.results.map( function ( result, i ) {
+    return ( peek.results.length > 1 ? '[' + i + ']: ' : '' ) + JSON.stringify( result )
+  }).join( '\n' ) + '\n\n';
+}
+
+function formatCode ( code ) {
+  return '```js\n' + code.trim() + '\n```\n\n';
+}
+
+var code = fs.readFileSync(file, 'utf-8')
+nodebooks.run(code, function (result) {
+  var lines = code.split('\n');
+  var peeks = []
+
+  // TODO different outputters
+  Object.keys(result).forEach(function (uid) {
+    peeks.push(result[uid]);
+  })
+
+  peeks.sort(function (a, b) {
+    return a.line - b.line;
+  })
+
+  var block = [];
+  var blocks = [];
+  var nextPeek = peeks.shift();
+  var i;
+  for ( i = 0; i < lines.length; i += 1 ) {
+    if ( !nextPeek || i < nextPeek.line ) {
+      block.push( lines[i] );
+    } else {
+      if ( block.length ) {
+        blocks.push( formatCode( block.join('\n') ) );
+        block = [ lines[i] ];
+      }
+
+      blocks.push( formatPeek( nextPeek ) );
+      nextPeek = peeks.shift();
+    }
+  }
+
+  blocks.push( formatCode( block.join( '\n' ) ) )
+
+  console.log( blocks.join( '\n' ).trim() )
+})
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var io = require('indian-ocean')
 
-var display = require('./nodebooks.js')
 var data = io.readDataSync('data/test.csv')
 
 var numbs = [1,2,3]
 
 display(data)
 display(numbs)
+
+numbs.forEach(function (num) {
+	display(num*num);
+});
